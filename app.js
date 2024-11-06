e0f2dde068f6bcef73fa214707ff34faf2176d90
Imi Nkateko <shikuhelenkateko@gmail.com>
2024-11-06 20:46:42 +0200

    initialize node modules and tackling fibonacci using iteration

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+#!/usr/bin/node
 
+function fibs(n) {
+   if (n === 0) return [];
+   if (n === 1) return [0];
+
+   let fibbonacci = [0, 1];
+
+   for (let i = 2; i < n; i++) {
+    fibbonacci.push(fibbonacci[i - 1] + fibbonacci[i -2]);
+   }
+   return fibbonacci;
+}
+
+console.log(fibs(8));
